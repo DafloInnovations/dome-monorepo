33a91cc2f58624c3475d566ab9624287fe355cb3
tierxdcs <nithin.gangadhar@tierxdcs.com>
2026-06-03 20:53:10 -0400

    any left

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@dome/ui", "@dome/types", "@dome/utils", "@dome/api-client"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.amazonaws.com" },
